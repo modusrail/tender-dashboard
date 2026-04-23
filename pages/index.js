17f072929c1d2ae6d602c09c77bbfe861695080d
modusrail <andrewpickering@modusrail.co.uk>
2026-04-23 13:18:35 +0100

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>🚆 Tender Watch Dashboard</h1>
-      <p>It’s working 🎉</p>
-    </div>
-  );
-}
