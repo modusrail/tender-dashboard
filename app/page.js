c34dbcba5159db6bb753488fee55b5b7d4ae2f9b
modusrail <andrewpickering@modusrail.co.uk>
2026-04-23 13:01:49 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>🚆 Tender Watch Dashboard</h1>
+      <p>It’s working 🎉</p>
+    </div>
+  );
+}
